d43cbf4c07c0cacfcafa861818228e6e4921e7ba
LGarciaGonz <190077@iessanalberto.com>
2024-03-01 10:46:32 +0100

    commit controlar que el nombre exista

--- src/main/java/database/ejercicio2/listarContactos.java
+++ src/main/java/database/ejercicio2/listarContactos.java
@@ -15,21 +15,27 @@ public class listarContactos {
                     \t\t\tAGENDA
                     ----------------------------------------------""");
 
-            // Preparar la consulta para listar los nombres.
+            // Preparar la consulta para listar los nombres y teléfonos.
             String listCont = "SELECT A.NOMBRE, A.TELEF FROM AGENDA A";
             Statement stmt = miCon.createStatement();
             ResultSet rs = stmt.executeQuery(listCont);
 
-            // Mostrar los nombres de los contactos obtenidos.
+            // Mostrar los nombres de los contactos y sus teléfonos obtenidos.
             while (rs.next()){
-                String contRs = rs.getString("A.NOMBRE");
-                int telefonos = rs.getInt("A.TELEF");
-                System.out.println("\n- " + contRs + ": " + telefonos);
+                String contRs = rs.getString("NOMBRE");
+                Array telefonoArray = rs.getArray("TELEF");
+                Object[] telefonos = (Object[]) telefonoArray.getArray();
+
+                System.out.println("\n- " + contRs + ": ");
+                for (Object telefono : telefonos) {
+                    System.out.println("\t" + telefono.toString());
+                }
             }
 
         } catch (SQLException e) {
             // Manejar errores de SQL
-            System.err.println("\n>>> No se han podido listar los contactos. ERROR: " + e.getMessage());
+            System.err.println("\n>>> No se han podido listar los contactos. ERROR: ");
+            e.printStackTrace();
         }
 
     }
